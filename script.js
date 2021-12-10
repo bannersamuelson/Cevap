7836140645fd0205647b93b7f1362a65e755db95
Banner Samuelson <banner@Banners-MacBook-Air.local>
2021-12-10 16:01:32 -0500

    added axios site

---
+++ script.js
@@ -0,0 +1,33 @@
+// const express = require('express');
+// const request = require('request');
+// const app = express();
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
+// app.get('', (req, res) => {
+//   request(
+//     { url: 'https://www.easports.com/fifa/ultimate-team/api/fut/item' },
+//     (error, response, body) => {
+//       if (error || response.statusCode !== 200) {
+//         return res.status(500).json({ type: 'error', message: err.message });
+//       }
+
+//       res.json(JSON.parse(body));
+//     }
+//   )
+// });
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+
+async function getGame() {
+
+  const url = `https://www.freetogame.com/api/games`;
+  const res = await axios.get(url);
+  console.log(res);
+  // const countryData = res.data;
+}
+
+getGame();
