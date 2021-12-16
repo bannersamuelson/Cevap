3999639ed3bd5c075deacddba91d12e71749a164
Banner Samuelson <banner@Banners-MacBook-Air.local>
2021-12-16 18:18:49 -0500

    done

--- assets/script.js
+++ assets/script.js
@@ -325,7 +325,6 @@ function showGameData(game) {
       fav.style.borderBottom = "solid 1px #d5d5d5"
       fav.style.borderMargin = "margin: 0 20px 10px 20px;"
       home.style.borderBottom = "none";
-      footer.style.positon = "0px";
 
 
 
